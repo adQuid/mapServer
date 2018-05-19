0aa046de731ce1c0c3cf3520d8849f576941a173
Brian <cricketfeir@gmail.com>
2018-05-19 15:26:18 -0400

    changed up controllers

--- app/v1/map/MapController.java
+++ app/v1/map/MapController.java
@@ -5,7 +5,14 @@ import play.mvc.*;
 public class MapController extends Controller{
 
 	public Result getMap(){
-		return ok("test");
+		int[][] grid = new int [10][10];
+		
+		for(int row=0; row < grid.length; row++){
+			for(int col=0; col < grid[0].length; col++){
+				grid[row][col] = (int)(Math.random() * 3);
+			}
+		}
+		return ok(jsonifyGrid(grid));
 	}
 	
 	public Result getMapSlowly(){
@@ -14,6 +21,19 @@ public class MapController extends Controller{
 			retval += i;
 		}
 		return ok("result is "+retval);
+	}
+	
+	private String jsonifyGrid(int[][] grid){
+		String retval = "";
+		
+		for(int row=0; row < grid.length; row++){
+			for(int col=0; col < grid[0].length; col++){
+				retval += grid[row][col];
+				retval += ",";
+			}
+			retval=retval.substring(0, retval.length()-1)+";";
+		}
+		return retval;
 	}
 	
 }
