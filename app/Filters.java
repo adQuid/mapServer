781185517bb00fa5ec1f6bd61caa7ff3aeb1181e
Brian <cricketfeir@gmail.com>
2018-05-19 17:01:03 -0400

    changed cors

--- app/Filters.java
+++ app/Filters.java
@@ -1,23 +1,14 @@
-import play.api.http.EnabledFilters;
-import play.filters.cors.CORSFilter;
-import play.http.DefaultHttpFilters;
 import play.mvc.EssentialFilter;
-
+import play.filters.cors.CORSFilter;
+import play.http.HttpFilters;
 import javax.inject.Inject;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
-public class Filters extends DefaultHttpFilters {
+public class Filters implements HttpFilters {
 
     @Inject
-    public Filters(EnabledFilters enabledFilters, CORSFilter corsFilter) {
-        super(combine(enabledFilters.asJava().getFilters(), corsFilter));
-    }
+    CORSFilter corsFilter;
 
-    private static List<EssentialFilter> combine(List<EssentialFilter> filters, EssentialFilter toAppend) {
-        List<EssentialFilter> combinedFilters = new ArrayList<>(filters);
-        combinedFilters.add(toAppend);
-        return combinedFilters;
+    public EssentialFilter[] filters() {
+        return new EssentialFilter[] { corsFilter.asJava() };
     }
 }
