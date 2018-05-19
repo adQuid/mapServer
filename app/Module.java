95564c04a39174d4882edf7d3ccf44ae26e9bb75
Brian <cricketfeir@gmail.com>
2018-05-19 13:50:27 -0400

    removed import

--- app/Module.java
+++ app/Module.java
@@ -4,8 +4,6 @@ import com.codahale.metrics.Slf4jReporter;
 import com.google.inject.AbstractModule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import v1.post.PostRepository;
-import v1.post.JPAPostRepository;
 
 import javax.inject.Provider;
 import java.util.concurrent.TimeUnit;
