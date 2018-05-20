51d1013abfc08f386811b38adfb86483907774fb
Brian <cricketfeir@gmail.com>
2018-05-20 10:42:22 -0400

    trying to get cors to work day 2

--- app/v1/map/MapController.java
+++ app/v1/map/MapController.java
@@ -12,7 +12,7 @@ public class MapController extends Controller{
 				grid[row][col] = (int)(Math.random() * 3);
 			}
 		}
-		return ok(jsonifyGrid(grid)).withHeaders("access-control-allow-origin" -> "*");
+		return Ok(jsonifyGrid(grid));
 	}
 	
 	public Result getMapSlowly(){
